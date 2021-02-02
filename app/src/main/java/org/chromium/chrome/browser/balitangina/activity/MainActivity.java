6f800b7b55269084223c92156cffb130a01787a2
“Unexxxx” <kentonyx@gmail.com>
2021-02-02 09:47:52 +0800

    Added bottom navigation bar and its functionalities

--- app/src/main/java/org/chromium/chrome/browser/balitangina/activity/MainActivity.java
+++ app/src/main/java/org/chromium/chrome/browser/balitangina/activity/MainActivity.java
@@ -2,6 +2,8 @@ package org.chromium.chrome.browser.balitangina.activity;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -13,10 +15,14 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import org.chromium.chrome.browser.balitangina.bottomnavigationview.fragments.FragmentEntertainment;
+import org.chromium.chrome.browser.balitangina.bottomnavigationview.fragments.FragmentHome;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import org.chromium.chrome.browser.balitangina.R;
 import org.chromium.chrome.browser.balitangina.adapter.MainArticleAdapter;
+import org.chromium.chrome.browser.balitangina.bottomnavigationview.fragments.FragmentScience;
+import org.chromium.chrome.browser.balitangina.bottomnavigationview.fragments.FragmentTechnology;
 import org.chromium.chrome.browser.balitangina.model.Article;
 import org.chromium.chrome.browser.balitangina.model.ResponseModel;
 import org.chromium.chrome.browser.balitangina.rests.APIInterface;
@@ -44,8 +50,9 @@ public class MainActivity extends AppCompatActivity {
         final RecyclerView mainRecycler = findViewById(R.id.activity_main_rv);
         final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mainRecycler.setLayoutManager(linearLayoutManager);
+
         final APIInterface apiService = ApiClient.getClient().create(APIInterface.class);
-        Call<ResponseModel> call = apiService.getLatestNews("ph", API_KEY);
+        Call<ResponseModel> call = apiService.getLatestNews("ph", "", API_KEY);
         call.enqueue(new Callback<ResponseModel>() {
             @Override
             public void onResponse(retrofit2.Call<ResponseModel> call, retrofit2.Response<ResponseModel> response) {
@@ -66,28 +73,101 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.nav_top_stories:
-                        Toast.makeText(MainActivity.this, "TOP STORIES", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.nav_news:
-                        Toast.makeText(MainActivity.this, "NEWS", Toast.LENGTH_SHORT).show();
-                        break;
-                    case R.id.nav_entertainment:
-                        Toast.makeText(MainActivity.this, "ENTERTAINMENT", Toast.LENGTH_SHORT).show();
-                        break;
+                        openFragment(new FragmentHome());
 
+                        final APIInterface apiService = ApiClient.getClient().create(APIInterface.class);
+                        Call<ResponseModel> call = apiService.getLatestNews("ph", "", API_KEY);
+                        call.enqueue(new Callback<ResponseModel>() {
+                            @Override
+                            public void onResponse(retrofit2.Call<ResponseModel> call, retrofit2.Response<ResponseModel> response) {
+                                if (response.body().getStatus().equals("ok")) {
+                                    List<Article> articleList = response.body().getArticles();
+                                    if (articleList.size() > 0) {
+                                        final MainArticleAdapter mainArticleAdapter = new MainArticleAdapter(articleList, MainActivity.this);
+                                        mainArticleAdapter.setOnRecyclerViewItemClickListener(MainActivity.this);
+                                        mainRecycler.setAdapter(mainArticleAdapter);
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(retrofit2.Call<ResponseModel> call, Throwable t) {
+                                Log.e("out", t.toString());
+                            }
+
+                        });
+                        return true;
+                    case R.id.nav_tech:
+                        openFragment(new FragmentTechnology());
+
+                        final APIInterface apiService1 = ApiClient.getClient().create(APIInterface.class);
+                        Call<ResponseModel> call1 = apiService1.getLatestNews("ph", "technology", API_KEY);
+                        call1.enqueue(new Callback<ResponseModel>() {
+                            @Override
+                            public void onResponse(retrofit2.Call<ResponseModel> call, retrofit2.Response<ResponseModel> response) {
+                                if (response.body().getStatus().equals("ok")) {
+                                    List<Article> articleList = response.body().getArticles();
+                                    if (articleList.size() > 0) {
+                                        final MainArticleAdapter mainArticleAdapter = new MainArticleAdapter(articleList, MainActivity.this);
+                                        mainArticleAdapter.setOnRecyclerViewItemClickListener(MainActivity.this);
+                                        mainRecycler.setAdapter(mainArticleAdapter);
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(retrofit2.Call<ResponseModel> call, Throwable t) {
+                                Log.e("out", t.toString());
+                            }
+                        });
+                        return true;
+                    case R.id.nav_science:
+                         openFragment(new FragmentScience());
+
+                         final APIInterface apiService2 = ApiClient.getClient().create(APIInterface.class);
+                         Call<ResponseModel> call2 = apiService2.getLatestNews("ph", "science", API_KEY);
+                         call2.enqueue(new Callback<ResponseModel>() {
+                             @Override
+                             public void onResponse(retrofit2.Call<ResponseModel> call, retrofit2.Response<ResponseModel> response) {
+                                 if (response.body().getStatus().equals("ok")) {
+                                     List<Article> articleList = response.body().getArticles();
+                                     if (articleList.size() > 0) {
+                                         final MainArticleAdapter mainArticleAdapter = new MainArticleAdapter(articleList, MainActivity.this);
+                                         mainArticleAdapter.setOnRecyclerViewItemClickListener(MainActivity.this);
+                                         mainRecycler.setAdapter(mainArticleAdapter);
+                                     }
+                                 }
+                             }
+
+                             @Override
+                             public void onFailure(retrofit2.Call<ResponseModel> call, Throwable t) {
+                                 Log.e("out", t.toString());
+                             }
+                         });
+                        return true;
+//                    case R.id.nav_science:
+//                         openFragment(new FragmentScience());
+//                        return true;
+//                    case R.id.nav_entertainment:
+//                         openFragment(new FragmentEntertainment());
+//                        return true;
                 }
-                return true;
+                return false;
             }
         });
     }
-
-
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 
     public void onItemClick(int position, View view) {
         switch (view.getId()) {
